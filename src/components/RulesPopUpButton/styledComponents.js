485d82e2362efb77d54d39bcb43b66666aa0d1c6
Vardhan077 <vardhanmarriwada777@gmail.com>
2024-01-10 21:03:12 +0530

    first commit

---
+++ src/components/RulesPopUpButton/styledComponents.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components'
+
+export const PopUpContainer = styled.div`
+  align-self: flex-end;
+`
+export const RulesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height:500px;
+  width:auto;
+  align-items: center;
+`
+export const RulesButton = styled.button`
+  background-color: #ffffff;
+  font-family: 'Bree Serif';
+  height:30px;
+  width:auto;
+  margin-top:35px;
+  margin-right:25px;
+  color: #223a5f;
+  border: none;
+  border-radius: 5px;
+  padding: 4px 12px;
+  margin-right: 10px;
+  cursor: pointer;
+  outline: none;
+`
+
+export const CloseButton = styled.button`
+  border: none;
+  align-self: flex-end;
+  padding: 10px;
+`
+
+export const RulesImage = styled.img`
+  width: 480px;
+  height:400px;
+  margin: 20px 40px;
+`
